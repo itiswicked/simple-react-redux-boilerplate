f3c9707ce5f3d1d7198e4832d7d61bb3dcf77b14
Nathan Bourke <nathanrbourke@gmail.com>
2016-08-12 12:19:51 -0400

    import stuff, and render hello world

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <p>"Hello, World!"</p>,
+  document.getElementById('app')
+)
